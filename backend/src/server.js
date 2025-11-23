c433d95bae8d21fe76b220b13cbb3c3a8414c69d
MatheusFranciscoLS <matheusfran.ls@gmail.com>
2025-11-23 12:37:14 -0300

    23/11/2025

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,13 @@
 import app from "./app.js";
+import { PrismaClient } from "@prisma/client";
+import { execSync } from "child_process";
+
+try {
+    console.log("Gerando Prisma Client...");
+    execSync("npx prisma generate", { stdio: "inherit" });
+} catch (err) {
+    console.error("Erro ao gerar Prisma Client", err);
+}
 
 const PORT = process.env.PORT || 3001;
 
