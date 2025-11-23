8856eb544217a9d2dcfd324bd7a2688a1c3a5985
MatheusFranciscoLS <matheusfran.ls@gmail.com>
2025-11-23 11:45:05 -0300

    Configurando API URL dinâmica

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:3001/api';
+
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  baseURL: baseURL,
 });
 
 // 1. Interceptor de ENVIO
