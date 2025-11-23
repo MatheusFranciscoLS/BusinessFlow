74f038075239a0968f4d878914c6edfc47fafb09
MatheusFranciscoLS <matheusfran.ls@gmail.com>
2025-11-23 13:55:15 -0300

    23/11/2025

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_API_URL || 'https://businessflow-backend-ixh7.onrender.com';
+const baseURL = process.env.REACT_APP_API_URL || 'https://businessflow-backend.vercel.app';
 
 const api = axios.create({
   baseURL: baseURL,
