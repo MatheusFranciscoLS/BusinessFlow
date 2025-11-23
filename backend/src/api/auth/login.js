58694215a1a3978649b4857b845d95d64b91dab5
MatheusFranciscoLS <matheusfran.ls@gmail.com>
2025-11-23 13:43:27 -0300

    Configurando Backend para Vercel

---
+++ backend/src/api/auth/login.js
@@ -0,0 +1,30 @@
+import prisma from "../../lib/prisma.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST")
+    return res.status(405).json({ error: "Método não permitido" });
+
+  const { email, password } = req.body;
+
+  const user = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (!user) {
+    return res.status(400).json({ error: "Credenciais inválidas" });
+  }
+
+  const ok = await bcrypt.compare(password, user.password);
+
+  if (!ok) {
+    return res.status(400).json({ error: "Credenciais inválidas" });
+  }
+
+  const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+
+  return res.status(200).json({ token });
+}
