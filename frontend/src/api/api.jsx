981c2cb1d7a4a60902df5513d6e66836104f30a1
MatheusFranciscoLS <matheusfran.ls@gmail.com>
2025-11-23 14:21:07 -0300

    23/11/2025

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: "https://businessflow-backend-ixh7.onrender.com/api",
 });
 
 // Adiciona token automaticamente
