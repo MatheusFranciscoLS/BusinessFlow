0ab745f2ec71ab90240091e3da228112045354af
MatheusFranciscoLS <matheusfran.ls@gmail.com>
2025-11-23 14:41:03 -0300

    23/11/2025

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://businessflow-backend.vercel.app/api",
+  baseURL: "https://businessflow.onrender.com/api",
 });
 
 // Adiciona token automaticamente
