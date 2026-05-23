4208973862bbdff81371bc903652e97af8b6af7d
MatheusFranciscoLS <matheusfran.ls@gmail.com>
2026-05-23 16:57:34 -0300

    Fix: Remove @import do styled-components para impedir quebra silenciosa de CSS

--- frontend/src/styles/global.js
+++ frontend/src/styles/global.js
@@ -1,8 +1,6 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap');
-
   * {
     margin: 0;
     padding: 0;
@@ -11,7 +9,7 @@ export default createGlobalStyle`
   }
 
   body {
-    background: #f7fafc; /* Cor de fundo moderna */
+    background: #f7fafc;
     -webkit-font-smoothing: antialiased;
     color: #1a202c;
   }
