725d136731790b9404de914aff238ef692d188b4
MatheusFranciscoLS <matheusfran.ls@gmail.com>
2026-05-31 10:08:01 -0300

    fix ofx

--- backend/src/routes/ofx.routes.js
+++ backend/src/routes/ofx.routes.js
@@ -2,16 +2,18 @@ import { Router } from "express";
 import * as ofxController from "../controllers/ofx.controller.js";
 import { authMiddleware } from "../middlewares/auth.js";
 import { companyMiddleware } from "../middlewares/company.js";
-import { upload } from "../config/multer.js";
+import multer from "multer"; // Importamos o multer diretamente aqui!
 
 const router = Router();
 
-// Apenas Gestores logados podem subir ficheiros OFX
+// 🔥 Configuramos o Multer para NÃO gravar no disco do Render, apenas guardar na Memória (Buffer)
+const uploadMemory = multer({ storage: multer.memoryStorage() });
+
 router.post(
   "/parse",
   authMiddleware,
   companyMiddleware,
-  upload.single("file"), // Middleware que intercepta o ficheiro
+  uploadMemory.single("file"), // Usamos o nosso multer de memória
   ofxController.parse,
 );
 
